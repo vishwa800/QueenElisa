04de4bbd69bcaa0d456de2a9ccc4abfabf081199
vishwa800 <165990802+vishwa800@users.noreply.github.com>
2024-12-08 07:03:50 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,7 +19,7 @@ module.exports = {
 
 Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ`,
   ALIVE_MEDIA: 'https://telegra.ph/file/d842677f522b6bef4dfe6.jpg',
-  MONGODB_URL: '', 
+  MONGODB_URL: 'mongodb+srv://Xcelsama:Xcel@xcelsama.qpklf.mongodb.net/?retryWrites=true&w=majority&appName=Xcelsama', 
   PREFIX: '.$&',
   LANGUAGE: 'EN',
   PORT: 3000,
